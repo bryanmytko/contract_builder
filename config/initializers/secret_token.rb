f7de472ab0239b0d31884b6388dbe89c4795eddf
Bryan Mytko <bryanmytko@gmail.com>
2012-04-18 12:25:12 -0400

    Initial Commit. Authorization complete. Login page skinned.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '30b91995cbda37d454f284e123951b65460f7c72ef9cc866fea38a53b8a367973b8c6d50c52fb3a89a4a2f4010973201ef675eec71d193f7c630a0b751d9344e'
