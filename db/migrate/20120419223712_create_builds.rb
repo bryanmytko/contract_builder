e0ad6364112987787abbf6fb88c1d21154ef7888
Bryan Mytko <bryanmytko@gmail.com>
2012-04-19 19:43:07 -0400

    Build Controller. Hourly rate update works

---
+++ db/migrate/20120419223712_create_builds.rb
@@ -0,0 +1,11 @@
+class CreateBuilds < ActiveRecord::Migration
+  def change
+    create_table :builds do |t|
+      t.integer :contract_id
+      t.string :saved_state
+      t.string :hourly_rate
+
+      t.timestamps
+    end
+  end
+end
