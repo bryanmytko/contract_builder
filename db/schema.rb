2209813973069f988edc2635f0350a10ea4c3764
Bryan Mytko <bryanmytko@gmail.com>
2012-05-23 16:03:26 -0400

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120521203627) do
+ActiveRecord::Schema.define(:version => 20120523192730) do
 
   create_table "builds", :force => true do |t|
     t.integer  "contract_id"
-    t.string   "saved_state"
+    t.text     "saved_state"
     t.string   "hourly_rate"
     t.string   "cms_type"
     t.datetime "created_at",  :null => false
