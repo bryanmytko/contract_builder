4a194dc2d20a56501fe0471af2351604dbed6bc6
Bryan Mytko <bryanmytko@gmail.com>
2012-06-12 19:36:44 -0400

    db9

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,11 +2,7 @@ class DashboardsController < ApplicationController
     
     def index
        @title = 'Dashboard'
-     if(Contract.find_by_id(session[:user_id]))
-       @webready = Contract.find(:all, 
-                                 :conditions => ["contract_type = ? and user_owner = ?",
-                                                 'webready', session[:user_id]])
-      end
+    
        if(Contract.find_by_id(session[:user_id]))
          @professional = Contract.find(:all, 
                                      :conditions => ["contract_type = ? and user_owner = ?",
