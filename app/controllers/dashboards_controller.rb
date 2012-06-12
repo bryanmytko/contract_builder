12ae4efd0d8e18ee7d928c2e26c79f465a75524d
Bryan Mytko <bryanmytko@gmail.com>
2012-06-12 19:42:48 -0400

    db10

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,7 +6,7 @@ class DashboardsController < ApplicationController
        if(Contract.find_by_id(session[:user_id]))
          @professional = Contract.find(:all, 
                                      :conditions => ["contract_type = ? and user_owner = ?",
-                                                     'professional', session[:user_id]])
+                                                     'professional', session[:user_id].to_s])
         end
         session_check('index')
     end
